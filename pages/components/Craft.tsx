92aae483564d412dbf57d997b2f203d86a9c2623
lissalad <elissalaymon@gmail.com>
2023-03-22 15:09:47 -0700

    tutorial linked ugly

--- pages/components/Craft.tsx
+++ pages/components/Craft.tsx
@@ -6,14 +6,22 @@ export default function Craft({ craft }: { craft: Item }) {
   const image = `/images/${craft.image}`;
 
   return (
-    <div className="flex flex-col flex-nowrap hover:scale-[0.96] transition-all aspect-square">
-      {/* flex grow */}
-      <img
-        className="w-full h-full object-cover rounded-xl shadow-xl hover:shadow-3xl aspect-square"
-        alt={craft.title}
-        src={image}
-      />
-      <p className="capitalize pt-2">{craft.title}</p>
+    <div className="flex flex-col hover:scale-[0.96] transition-all aspect-square items-center justify-center relative group duration-200">
+      <div className="relative w-full h-full overflow-hidden rounded-xl shadow-xl hover:shadow-3xl transition-all duration-200">
+        <img
+          className="w-full h-full object-cover group-hover:blur-sm group-hover:brightness-[50%] transition-all duration-200"
+          alt={craft.title}
+          src={image}
+        />
+      </div>
+      <p className="capitalize p-2 w-full text-left">{craft.title}</p>
+      <a
+        href={craft.link}
+        target="_blank"
+        className="invisible group-hover:visible py-3 px-5 rounded-xl bg-rose-500 text-rose-50 shadow-xl absolute transition-all duration-400 border-4 border-rose-200/60 hover:bg-rose-400"
+      >
+        Try it Out
+      </a>
     </div>
   );
 }
--- pages/components/Sidebar.tsx
+++ pages/components/Sidebar.tsx
@@ -29,26 +29,29 @@ export default function Sidebar({
       className="flex flex-row gap-2 h-fit capitalize whitespace-nowrap text-sm"
       key={key}
     >
-      <input
-        type="checkbox"
-        name={m}
-        value={m}
-        checked={selectedMaterials.includes(m)}
-        onChange={() => handleCheckboxChange(m)}
-      />
-      <p>{m}</p>
+      <label htmlFor={m} className="flex flex-row items-center">
+        <input
+          type="checkbox"
+          id={m}
+          name={m}
+          value={m}
+          checked={selectedMaterials.includes(m)}
+          onChange={() => handleCheckboxChange(m)}
+        />
+        <span className="mx-2">{m}</span>
+      </label>
     </li>
   ));
 
   if (menuOpen) {
     return (
-      <div className="w-full sm:w-fit sm:static fixed bg-rose-50 shadow-rose-300 shadow-md min-h-screen">
+      <div className="w-full sm:w-fit sm:static fixed bg-rose-50 shadow-rose-300 shadow-md min-h-screen text-rose-900">
         <div className="sticky top-0 py-4">
-          <p className="text-md font-semibold border-b border-black pb-4 mb-4 mx-4">
+          <p className="text-md font-semibold border-b-2 border-rose-200 pb-4 mb-4 mx-4">
             Supply Closet
           </p>
           <div className="px-6">
-            <ul className="flex flex-col pt-2 pb-6">{materialsList}</ul>
+            <ul className="flex flex-col pt-2 pb-6 gap-1">{materialsList}</ul>
             <div className="flex flex-col items-start">
               <button className={buttonStyles} onClick={selectAll}>
                 select all
